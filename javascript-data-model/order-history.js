95dd780ed267db9996131d308302b1370086022d
Tim Silva <tim17silva@gmail.com>
2023-05-09 20:45:11 +0000

    Completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,53 @@
+const orderHistory = [
+  {
+    dateOfOrder: '08/04/2020',
+    dateOfDelivery: '08/08/2020',
+    orderNo: '114-3941689-8772232',
+    total: 34.0,
+    returnDate: '09/07/2020',
+    items: [
+      {
+        title: 'JavaScript for impatient programmers',
+        author: 'Dr. Axel Rauschmayer',
+        price: 31.55,
+      },
+    ],
+  },
+  {
+    dateOfOrder: '07/19/2020',
+    dateOfDelivery: '07/20/2020',
+    orderNo: '113-9984268-1280257',
+    total: 44.53,
+    returnDate: '08/19/2020',
+    items: [
+      {
+        title: 'The Timeless Way of Building',
+        author: 'Christopher Alexander',
+        price: 41.33,
+      },
+    ],
+  },
+  {
+    dateOfOrder: '04/07/2020',
+    dateOfDelivery: '07/07/2020',
+    orderNo: '114-2875557-9059409',
+    total: 17.22,
+    returnDate: '08/05/2020',
+    items: [{ title: 'Gamecube Controller Adapter', author: '', price: 15.98 }],
+  },
+  {
+    dateOfOrder: '07/03/2020',
+    dateOfDelivery: '07/05/2020',
+    orderNo: '113-2883177-2648248',
+    total: 138.93,
+    returnDate: '08/04/2020',
+    items: [
+      { title: 'GameCube Controller', author: '', price: 94.95 },
+      {
+        title: 'The Art of Sql',
+        author: 'Stephanie Faroult',
+        price: 33.99,
+      },
+    ],
+  },
+];
