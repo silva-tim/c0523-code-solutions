b7511abe474689d8002f8a197dd8ccd356ff8909
Tim Silva <tim17silva@gmail.com>
2023-05-10 20:52:00 +0000

    Fixed exclusion of 14 from base

--- javascript-conditionals/main.js
+++ javascript-conditionals/main.js
@@ -62,7 +62,7 @@ console.log(
 function categorizeAcidity(pH) {
   if (pH < 7 && pH >= 0) {
     return 'acid';
-  } else if (pH > 7 && pH < 14) {
+  } else if (pH > 7 && pH <= 14) {
     return 'base';
   } else if (pH === 7) {
     return 'neutral';
