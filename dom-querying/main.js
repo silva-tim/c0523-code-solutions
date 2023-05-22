0bf43264ca07b9acd181e42d4bb10238e5eb9146
Tim Silva <tim17silva@gmail.com>
2023-05-22 16:52:37 +0000

    Completed dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,17 @@
+console.log('hello, world');
+
+console.log('$heading', document.querySelector('h1'));
+
+console.dir(document.querySelector('h1'));
+
+console.log('$explanation', document.querySelector('#explanation'));
+
+console.dir(document.querySelector('#explanation'));
+
+console.log('$hint', document.querySelector('.hint'));
+
+console.dir(document.querySelector('.hint'));
+
+console.log('$paragraphs', document.querySelectorAll('p'));
+
+console.log('$links', document.querySelectorAll('.example-link'));
