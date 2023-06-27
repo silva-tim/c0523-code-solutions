e18b3d6b14afc36a7a399df4c267ae6a41e46389
Tim Silva <tim17silva@gmail.com>
2023-06-27 17:20:11 +0000

    Testing resolving

--- react-events/my-app/src/CustomButton.jsx
+++
@@ -1,9 +0,0 @@
-import './CustomButton.css';
-
-export default function CustomButton({ color, text, onCustomClick }) {
-  return (
-    <button onClick={() => onCustomClick(text)} className={color}>
-      {text}
-    </button>
-  );
-}
