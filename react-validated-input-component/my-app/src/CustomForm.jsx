4efd0c6fe1b98ddb4eddb4df7f179dea439fd3cf
Tim Silva <tim17silva@gmail.com>
2023-06-28 17:55:52 +0000

    Fixed so it only uses one state variable.

--- react-validated-input-component/my-app/src/CustomForm.jsx
+++ react-validated-input-component/my-app/src/CustomForm.jsx
@@ -2,37 +2,37 @@ import { useState } from 'react';
 import './CustomForm.css';
 
 export default function CustomForm() {
-  const [valid, setValid] = useState('is-invalid');
-  const [validFeedback, setValidFeedback] = useState('A password is required.');
-  const [icon, setIcon] = useState('fa-x');
+  const [currentValue, setValue] = useState('');
+  let valid = 'is-invalid';
+  let validFeedback = 'A password is required.';
+  let icon = 'fa-x';
 
-  function handleInput(e) {
-    if (e.target.value === '') {
-      setValid('is-invalid');
-      setValidFeedback('A password is required.');
-      setIcon('fa-x');
-    } else if (e.target.value.length < 8) {
-      setValid('is-invalid');
-      setValidFeedback('Your password must be atleast 8 characters.');
-      setIcon('fa-x');
-    } else {
-      setValid('is-valid');
-      setIcon('fa-check');
-    }
+  if (currentValue === '') {
+    valid = 'is-invalid';
+    validFeedback = 'A password is required.';
+    icon = 'fa-x';
+  } else if (currentValue.length < 8) {
+    valid = 'is-invalid';
+    validFeedback = 'Your password must be atleast 8 characters.';
+    icon = 'fa-x';
+  } else {
+    valid = 'is-valid';
+    icon = 'fa-check';
   }
 
   return (
-    <form className="form-group m-5" novalidate>
+    <form className="form-group m-5" noValidate>
       <label htmlFor="password">Password</label>
       <div>
         <input
-          onChange={handleInput}
+          value={currentValue}
+          onChange={(e) => setValue(e.target.value)}
           className={`form-control ${valid} mr-2`}
           type="password"
           name="password"
           id="password"
         />
-        <i class={`fa-solid ${icon}`}></i>
+        <i className={`fa-solid ${icon}`}></i>
         <p className="invalid-feedback">{validFeedback}</p>
       </div>
     </form>
