2c7fb3de0b0ca390a6dc7f97433803fc6bb2099b
Tim Silva <tim17silva@gmail.com>
2023-07-06 19:49:17 +0000

    Committing to save work before fixing es6-promises

---
+++ es6-async-await/await.js
@@ -0,0 +1,37 @@
+import fetch from './fetch.js';
+
+// Keep track of app load time so each log message can be timed.
+// Log messages should all be approximately 1 second apart.
+const startTime = Date.now();
+const elapsed = () => `${Math.round((Date.now() - startTime) / 1000)}s -`;
+
+async function fetchOnce() {
+  const responseOnce = await fetch('foo/bar.html');
+  console.log(elapsed(), 'fetchOnce:', responseOnce);
+}
+
+async function fetchSeveral() {
+  const responseSeveral1 = await fetch('foo1/bar.html');
+  console.log(elapsed(), 'fetchSeveral1:', responseSeveral1);
+
+  const responseSeveral2 = await fetch('foo2/bar.html');
+  console.log(elapsed(), 'fetchSeveral2:', responseSeveral2);
+
+  const responseSeveral3 = await fetch('foo3/bar.html');
+  console.log(elapsed(), 'fetchSeveral3:', responseSeveral3);
+}
+
+async function fetchChained() {
+  const responseChained1 = await fetch('foo-chain/bar.html');
+  console.log(elapsed(), 'fetchSeveral1:', responseChained1);
+
+  const responseChained2 = await fetch(responseChained1);
+  console.log(elapsed(), 'fetchSeveral2:', responseChained2);
+
+  const responseChained3 = await fetch(responseChained2);
+  console.log(elapsed(), 'fetchSeveral3:', responseChained3);
+}
+
+fetchOnce()
+  .then(() => fetchSeveral())
+  .then(() => fetchChained());
