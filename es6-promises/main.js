f7dc100c86f944ecb047933c266bad956f35b4fc
Tim Silva <tim17silva@gmail.com>
2023-07-06 19:51:45 +0000

    Fixed promise so that then and catch are chained.

--- es6-promises/main.js
+++ es6-promises/main.js
@@ -1,5 +1,5 @@
 import takeAChance from './take-a-chance.js';
 
-const promise = takeAChance('Tim');
-promise.then((value) => console.log(value));
-promise.catch((error) => console.log(error.message));
+const promise = takeAChance('Tim')
+  .then((value) => console.log(value))
+  .catch((error) => console.log(error.message));
