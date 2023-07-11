388efd69b1f820299b86575a59b0a67cc5888985
Tim Silva <tim17silva@gmail.com>
2023-07-11 22:08:21 +0000

    Fixed Robert's requests, I think.

--- express-notes-api/server.ts
+++ express-notes-api/server.ts
@@ -10,7 +10,6 @@ type Data = {
   nextId: number;
   notes: Record<number, Note>;
 };
-let data: Data;
 
 type Err = {
   error: string;
@@ -26,7 +25,7 @@ app.use(express.json());
 // Returns list of notes in JSON
 app.get('/api/notes', async (req, res) => {
   try {
-    await readJSON();
+    const data = await readJSON();
     const notesList: Note[] = [];
     for (const key in data.notes) {
       notesList.push(data.notes[key]);
@@ -34,99 +33,107 @@ app.get('/api/notes', async (req, res) => {
     res.status(200).json(notesList);
   } catch (err) {
     console.error(err);
+    error.error = 'An unexpected error occurred.';
+    res.status(500).json(error);
   }
 });
 
 // Returns individual note based on id in JSON
 app.get('/api/notes/:id', async (req, res) => {
   try {
-    await readJSON();
-    const id: number = Number(req.params.id);
+    const data = await readJSON();
+    const id = Number(req.params.id);
     if (isNaN(id) || id < 1 || !Number.isInteger(id)) {
       error.error = `${req.params.id} is not a valid id number.`;
       res.status(400).json(error);
-      throw new Error(`${req.params.id} is not a valid id number.`);
+      return;
     }
     if (!data.notes[id]) {
       error.error = `Note with ID ${id} does not exist.`;
       res.status(404).json(error);
-      throw new Error(`Note with ID ${id} does not exist.`);
+      return;
     }
     res.status(200).json(data.notes[id]);
   } catch (err) {
     console.error(err);
+    error.error = 'An unexpected error occurred.';
+    res.status(500).json(error);
   }
 });
 
 // Creates a new note entry with a unique id and fills in the content given
 app.post('/api/notes', async (req, res) => {
   try {
-    await readJSON();
+    const data = await readJSON();
     if (!req.body.content) {
       error.error = 'Must include content property.';
       res.status(400).json(error);
-      throw new Error('Must include content property.');
+      return;
     }
     const id = data.nextId++;
     data.notes[id] = {
       id,
       content: req.body.content,
     };
-    await writeJSON(res);
+    await writeJSON(data);
     res.status(201).json(data.notes[id]);
   } catch (err) {
     console.error(err);
+    error.error = 'An unexpected error occurred.';
+    res.status(500).json(error);
   }
 });
 
 // Removes a note based on the id of the note
 app.delete('/api/notes/:id', async (req, res) => {
   try {
-    await readJSON();
-    const id: number = Number(req.params.id);
+    const data = await readJSON();
+    const id = Number(req.params.id);
     if (isNaN(id) || id < 1 || !Number.isInteger(id)) {
       error.error = `${id} is not a valid id number.`;
       res.status(400).json(error);
-      throw new Error(`${id} is not a valid id number.`);
+      return;
     }
     if (!data.notes[id]) {
       error.error = `Note with ID ${id} does not exist.`;
       res.status(404).json(error);
-      throw new Error(`Note with ID ${id} does not exist.`);
+      return;
     }
     delete data.notes[id];
-    await writeJSON(res);
+    await writeJSON(data);
     res.sendStatus(204);
   } catch (err) {
     console.error(err);
+    error.error = 'An unexpected error occurred.';
+    res.status(500).json(error);
   }
 });
 
 // Amends a pre-exisiting note with a new content field
 app.put('/api/notes/:id', async (req, res) => {
   try {
-    await readJSON();
-    const id: number = Number(req.params.id);
+    const data = await readJSON();
+    const id = Number(req.params.id);
     if (isNaN(id) || id < 1 || !Number.isInteger(id) || !req.body.content) {
       error.error = `${id} must be a valid number and content field must be included.`;
       res.status(400).json(error);
-      throw new Error(
-        `${id} must be a valid number and content field must be included.`
-      );
+      return;
     }
     if (!data.notes[id]) {
       error.error = `Note with ID ${id} does not exist.`;
       res.status(404).json(error);
-      throw new Error(`Note with ID ${id} does not exist.`);
+      return;
     }
     data.notes[id] = {
       id,
       content: req.body.content,
     };
-    await writeJSON(res);
+    await writeJSON(data);
     res.status(200).json(data.notes[id]);
   } catch (err) {
     console.error(err);
+    error.error = 'An unexpected error occurred.';
+    res.status(500).json(error);
   }
 });
 
@@ -135,20 +142,10 @@ app.listen(8080, () => {
 });
 
 async function readJSON() {
-  try {
-    data = JSON.parse(await readFile('data.json', 'utf8'));
-  } catch (err) {
-    console.log(err);
-  }
+  const data: Data = JSON.parse(await readFile('data.json', 'utf8'));
+  return data;
 }
 
-async function writeJSON(res: express.Response) {
-  try {
-    const newData: string = JSON.stringify(data, null, 2);
-    await writeFile('data.json', newData);
-  } catch (err) {
-    error.error = 'An unexpected error occurred.';
-    res.status(500).json(error.error);
-    throw new Error('An unexpected error occurred.');
-  }
+async function writeJSON(data: Data) {
+  await writeFile('data.json', JSON.stringify(data, null, 2));
 }
