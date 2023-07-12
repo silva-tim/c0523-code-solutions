5b2b193c528d0fe05c92349577c7e8a09682d139
Tim Silva <tim17silva@gmail.com>
2023-07-12 01:11:02 +0000

    Fixed issuses

--- express-static/server.js
+++ express-static/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-app.use('/public', express.static('public'));
+app.use(express.static('public'));
 
 app.listen(8080, () => {
   console.log('Express server listening on port 8080.');
