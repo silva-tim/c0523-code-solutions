748461842dd33a660990d9a6112656cfbf41f2b7
Tim Silva <tim17silva@gmail.com>
2023-07-24 23:02:12 +0000

    Completed stacks-3

--- data-structures/ts/stacks-3.ts
+++ data-structures/ts/stacks-3.ts
@@ -1,8 +1,27 @@
-// /* eslint-disable @typescript-eslint/no-empty-function */
-// import Stack from './lib/stack';
+/* eslint-disable @typescript-eslint/no-empty-function */
+import Stack from './lib/stack';
 
-// export function get2ndFromTop<T>(stack: Stack<T>): T | undefined {
-//   return undefined;
-// }
+export function get2ndFromTop<T>(stack: Stack<T>): T | undefined {
+  const temp = stack.pop();
+  if (temp === undefined) {
+    return undefined;
+  }
+  if (stack.peek() === undefined) {
+    stack.push(temp);
+    return undefined;
+  }
 
-// export function insertUnderTop<T>(stack: Stack<T>, value: T): void {}
+  const temp2 = stack.peek();
+  stack.push(temp);
+  return temp2;
+}
+
+export function insertUnderTop<T>(stack: Stack<T>, value: T): void {
+  const temp = stack.pop();
+  if (temp === undefined) {
+    return;
+  }
+
+  stack.push(value);
+  stack.push(temp);
+}
